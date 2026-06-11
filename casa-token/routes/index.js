1761b56dd48c7f6cab1c82d3660dc3945b9eb1f5
Codreanu Ruslan <codreanuruslan@users.noreply.github.com>
2026-06-11 12:46:55 +0300

    disable stale html caching

--- casa-token/routes/index.js
+++ casa-token/routes/index.js
@@ -3,12 +3,19 @@ const express = require('express');
 
 const router = express.Router();
 
+function sendHtml(res, fileName) {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('CDN-Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Cloudflare-CDN-Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.sendFile(path.join(__dirname, '..', 'views', fileName));
+}
+
 router.get(['/', '/buy'], (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+  sendHtml(res, 'index.html');
 });
 
 router.get('/miniapp', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'miniapp.html'));
+  sendHtml(res, 'miniapp.html');
 });
 
 module.exports = router;
